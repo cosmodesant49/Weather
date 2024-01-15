05f697f83ea89a95e8566ca36e0f701cc92148fb
cosmodesant49 <emilmaldybaev49@gmail.com>
2024-01-15 13:14:20 +0600

    Done_hw7

--- app/src/main/java/com/geeks/weather/MainActivity.kt
+++ app/src/main/java/com/geeks/weather/MainActivity.kt
@@ -3,6 +3,7 @@ package com.geeks.weather
 import android.net.DnsResolver.Callback
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.geeks.weather.databinding.ActivityMainBinding
 import retrofit2.Call
 import retrofit2.Response
@@ -13,7 +14,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        RetrofitService().api.getWeather("Bishkek")
+
+        binding.btnCity.setOnClickListener{
+        RetrofitService().api.getWeather(binding.etCity.text.toString())
             .enqueue(object : retrofit2.Callback<WeatherModel> {
                 override fun onResponse(
                     call: Call<WeatherModel>,
@@ -21,8 +24,12 @@ class MainActivity : AppCompatActivity() {
                 ) {
                     if (response.isSuccessful) {
                         response.body()?.let {
-                            binding.tvResult.text =
-                                "temp: ${it.main.temp} \n feelLike: ${it.main.feel_like} \n City ${it.name}"
+                            binding.tvCity.visibility = View.VISIBLE
+                            binding.cardView.visibility = View.VISIBLE
+                            binding.tvCity.text="${it.name}"
+                            binding.tvTemperatureResult.text="${it.main.temp}"
+                            binding.tvFeelLikeResult.text="${it.main.feel_like}"
+                            binding.tvWindResult.text="${it.wind.speed}"
                         }
                     }
                 }
@@ -31,5 +38,6 @@ class MainActivity : AppCompatActivity() {
                 }
 
             })
+            }
     }
 }
